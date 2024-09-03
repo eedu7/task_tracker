668bb7dfd8a5310b60a93b293656f4baeeaf8234
eedu7 <mueedahmad067@gmail.com>
2024-09-03 15:48:11 +0500

    FEAT: completed wokring on task handler

--- TaskHandler.py
+++ TaskHandler.py
@@ -1,7 +1,7 @@
 import json
-from enum import Enum
-from pprint import pprint
+# from pprint import pprint
 from datetime import datetime
+from enum import Enum
 
 
 class TaskStatus(Enum):
@@ -17,7 +17,7 @@ class TaskHandler:
 
     def add_task(self, task: str, description: str | None = None) -> str:
         task_id = self.__task_id()
-        task_data: dict[str, str] = {
+        task_data: dict[str, str | None] = {
             "id": task_id,
             "task": task,
             "description": description,
@@ -27,16 +27,48 @@ class TaskHandler:
         }
         data = self.__combine_data(task_data)
         self._create(data=data)
-        print(f"Task added successfully (ID: {task_id})")
-    
+        return f"Task added successfully (ID: {task_id})"
+
     def view_task(self, filter_by: str = "", filter_value: str = ""):
         data = self._read()
 
         if not filter_by or not filter_value:
             return data
-        
-        return list(filter(lambda x: x[filter_by] == filter_value,data))
 
+        return list(filter(lambda x: x[filter_by] == filter_value, data))
+
+    def delete_task(self, task_id: str) -> str:
+        data: list[dict] = self._read()
+        task: list[dict] = self.view_task(filter_by="id", filter_value=task_id)[0]
+
+        task_index: int = data.index(task)
+
+        del data[task_index]
+
+        self._create(data)
+
+        return f"Task deleted successfully! (ID: {task_id})"
+
+    def update_task(
+        self,
+        task_id: str,
+        updated_task: str | None = None,
+        updated_description: str | None = None,
+    ) -> str:
+        data = self._read()
+
+        task_to_update = self.view_task(filter_by="id", filter_value=task_id)[0]
+        ind: int = data.index(task_to_update)
+
+        if updated_task:
+            data[ind]["task"] = updated_task
+        if updated_description:
+            data[ind]["description"] = updated_description
+
+        data[ind]["updatedAt"] = str(datetime.now())
+
+        self._create(data)
+        return f"Task updated successfully! (ID: {task_id})"
 
     def _read(self) -> list[dict]:
         try:
@@ -45,6 +77,7 @@ class TaskHandler:
 
         except FileNotFoundError:
             self._create()
+            return []
         except Exception as e:
             print(f"Error occurred while reading JSON file: {e}")
 
@@ -69,6 +102,8 @@ class TaskHandler:
 
 
 if __name__ == "__main__":
-    task_obj: TaskHandler = TaskHandler()
-    
-    pprint(task_obj.view_task("task", "Task ID 15"))
+    obj: TaskHandler = TaskHandler()
+
+    obj.update_task(
+        "6", updated_task="Update task", updated_description="updated_description"
+    )
